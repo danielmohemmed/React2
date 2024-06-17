2470aba2e412b7045d1fa3f5802220defb10ca83
Daniel Mohammed <50109522+danielmohemmed@users.noreply.github.com>
2024-06-17 23:30:42 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,8 @@ import StoreContextProvider from "./context/StoreContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StoreContextProvider>
-      <App />
+     
     </StoreContextProvider> 
+     <App />
   </BrowserRouter>
 );
