aec67f97a64180229b9a5932ae4e7c81bd7ea335
Daniel Mohammed <50109522+danielmohemmed@users.noreply.github.com>
2024-06-18 00:53:06 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import StoreContextProvider from "./context/StoreContext.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import StoreContextProvider from './context/StoreContext.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <BrowserRouter>
     <StoreContextProvider>
       <App />
-    </StoreContextProvider> 
-    
-  </BrowserRouter>
+    </StoreContextProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
